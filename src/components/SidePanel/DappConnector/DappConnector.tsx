4488fbf925153b9ee89ece4308d7d89e60bffa2e
Dick Jones <jones.rrw@gmail.com>
2023-06-07 13:46:34 +0930

    add wallet connect toast

--- src/components/SidePanel/DappConnector/DappConnector.tsx
+++ src/components/SidePanel/DappConnector/DappConnector.tsx
@@ -3,31 +3,34 @@ import { useUserStore } from '../../../state/userStore'
 import style from './DappConnector.module.css'
 import * as O from 'fp-ts/Option'
 import { formatAddr } from '../../../utils/web3'
+import toast from 'react-hot-toast'
 
 export const DappConnector = () => {
-    
-    const userConnected = useUserStore(state => state.userConnected)
-    const connection = useUserStore(state => state.connectUser)
-    const disconnection = useUserStore(state => state.disconnectUser)
-    const userAddr = useUserStore(state => state.formattedUserAddress)
+	const userConnected = useUserStore((state) => state.userConnected)
+	const connection = useUserStore((state) => state.connectUser)
+	const disconnection = useUserStore((state) => state.disconnectUser)
+	const userAddr = useUserStore((state) => state.formattedUserAddress)
 
-    const handleClick = async () => {
-        if (!userConnected) connection()
-        else disconnection()
-    }
+	const handleClick = async () => {
+		if (!userConnected) {
+			connection()
+			toast.success('Wallet connected')
+		} else {
+			disconnection()
+			toast('Wallet disconnected')
+		}
+	}
 
-    const getWalletText = () => {
-        if (userConnected) return `Disconnect ${userAddr}`
-        else return 'Connect'
-    }
+	const getWalletText = () => {
+		if (userConnected) return `Disconnect ${userAddr}`
+		else return 'Connect'
+	}
 
-    return (
-        <div id={style['connect-button-container']}>
-            <div id={style['connect-button']} onClick={handleClick}>
-                <span>{getWalletText()}</span>
-            </div>
-        </div>
-    )
-
+	return (
+		<div id={style['connect-button-container']}>
+			<div id={style['connect-button']} onClick={handleClick}>
+				<span>{getWalletText()}</span>
+			</div>
+		</div>
+	)
 }
-
